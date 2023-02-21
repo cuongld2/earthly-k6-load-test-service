79e6efac06a4b46fc703c3e1943ed6d40b951de5
DonaldLe <ledinhcuong99@gmail.com>
2023-02-21 11:39:37 +0700

    init

---
+++ database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+
+USER = os.getenv('DB_USER')
+PASSWORD = os.environ.get('DB_PASSWORD')
+
+SQLALCHEMY_DATABASE_URL = (f"mysql+mysqlconnector://{USER}:{PASSWORD}@localhost:3306/blogFastAPI")
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,43 @@
+import http from 'k6/http';
+
+import { check, sleep } from 'k6';
+
+const serviceBaseUrl = 'http://localhost:8089'
+
+export const options = {
+
+    vus: 10,
+    stages:[
+        {duration: '30s', target: 1},
+        {duration: '1m30s', target: 10},
+        {duration: '20s', target: 0}
+    ],
+  
+  };
+
+  export default function () {
+
+    const responseAuthen = http.post(serviceBaseUrl+'/authenticate',JSON.stringify({
+        username: "earthly",
+        password: "12345"
+    }),{headers: { 'Content-Type': 'application/json' }})
+
+    const authenToken = responseAuthen.json().access_token
+
+    const params = {
+        headers: { 'Authorization': 'Bearer ' + authenToken },
+      };
+    
+    const responseCreateNewBlog = http.post(serviceBaseUrl+'/blog',JSON.stringify({
+        title:"Blog for Earthly",
+        content:"Blog for Earthly",
+        author:"Donald Le"
+    }),params)
+  
+    check(responseAuthen, { 'status was 200': (r) => r.status == 200 });
+
+    check(responseCreateNewBlog, { 'status was 200': (r) => r.status == 200 });
+  
+    sleep(1);
+  
+  }
